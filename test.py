3288855647dbfffe4f2e2ddc6438b4acadc909a9
HaidarFaruqi <haidarfaruqi126@gmail.com>
2025-10-24 10:46:32 +0700

    Feat: nambahin

---
+++ test.py
@@ -0,0 +1,10 @@
+def kecepatan_rata_rata(jarak, waktu):
+
+    if waktu <= 0:
+        raise ValueError("Waktu harus lebih besar dari nol.")
+    return jarak / waktu
+
+jarak = input("Masukkan jarak yang ditempuh (dalam kilometer): ")
+waktu = input("Masukkan waktu yang ditempuh (dalam jam): ")
+
+print("Kecepatan rata-rata:", kecepatan_rata_rata(float(jarak), float(waktu)), "km/jam")
